f92d9f71b03a5979f4934021c2d00d9065c0f5d2
Najim Uddin <najim7304@gmail.com>
2023-03-02 23:51:05 +0600

    client / added routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter as router, Routes, Route } from "react-router-dom";
 import { themeSettings } from "theme";
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return <div className="App">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route element={<Layout />}>
+        </Routes>
+      </ThemeProvider>
+    </router>
   </div>;
 }
 
