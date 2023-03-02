ff90ee2ddfc33a90fc21e72e5ebd5e0a51706f79
Najim Uddin <najim7304@gmail.com>
2023-03-02 23:57:47 +0600

    client / routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter as router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as router, Routes, Route, Navigate } from "react-router-dom";
 import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard"
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -13,6 +14,9 @@ function App() {
         <CssBaseline />
         <Routes>
           <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </ThemeProvider>
     </router>
